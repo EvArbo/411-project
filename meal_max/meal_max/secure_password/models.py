12ad2fe472f3788e7fc70f0a7c5be13d24f620e4
EvArbo <157144180+EvArbo@users.noreply.github.com>
2024-12-03 09:58:09 -0500

    Delete meal_max/meal_max/secure_password directory

--- meal_max/meal_max/secure_password/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-# Define the User model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
