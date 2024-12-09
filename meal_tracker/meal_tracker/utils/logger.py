a9821661d9f07bb8d1a83861bf6ebfbea5d7987c
Beren Yagmur Donmez <bydonmez@bu.edu>
2024-12-09 08:17:31 +0300

    Update logger.py

--- meal_tracker/meal_tracker/utils/logger.py
+++ meal_tracker/meal_tracker/utils/logger.py
@@ -5,19 +5,15 @@ from flask import current_app, has_request_context
 
 
 def configure_logger(logger):
-    logger.setLevel(logging.DEBUG)  # Set the desired logging level here
+    logger.setLevel(logging.DEBUG)  
 
-    # Create a console handler that logs to stderr
     handler = logging.StreamHandler(sys.stderr)
     handler.setLevel(logging.DEBUG)
 
-    # Create a formatter with a timestamp
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # Add the formatter to the handler
     handler.setFormatter(formatter)
 
-    # Add the handler to the logger
     logger.addHandler(handler)
 
     if has_request_context():
